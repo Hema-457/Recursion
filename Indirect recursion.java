bac2ed584c3f2afc9c3748735b0050e3b201e56f
Hema-457 <tamaranahema10@gmail.com>
2026-04-29 01:03:06 +0530

    Create Indirect recursion.java

---
+++ Indirect recursion.java
@@ -0,0 +1,22 @@
+class Main{
+    static void fun1(int n)
+    {
+        if(n>0)
+        {
+            System.out.println(n);
+            fun2(n-1);
+        }
+    }
+    static void fun2(int n)
+    {
+        if(n>0)
+        {
+            System.out.println(n);
+            fun1(n-1);
+        }
+    }
+    public static void main(String args[])
+    {
+        fun1(15);
+    }
+}
